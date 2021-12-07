449e992b8f41a37bfd9b8b7f993273368995d43c
Laura Santamaria <laura@nimbinatus.com>
2021-12-07 14:29:31 -0600

    fix(clean up): that fixed it, so clean up

--- infra/image/__main__.py
+++ infra/image/__main__.py
@@ -17,19 +17,13 @@ if 'CIRCLE_BRANCH' in os.environ and os.environ['CIRCLE_BRANCH'] != 'main':
 else:
     tag = 'latest'
 
-for root, dirs, files in os.walk(f'{path.parents[1]}/api'):
-    print(files)
-
 try:
     gunicorn_image = docker.Image(
         gimage,
         build=f'{path.parents[1]}/api',
-        # image_name=f'gcr.io/{pulumi.Config("gcp").require("project")}/{gimage}:{tag}'
-        image_name="testme:latest"
+        image_name=f'gcr.io/{pulumi.Config("gcp").require("project")}/{gimage}:{tag}'
     )
 except pulumi_docker.docker.ResourceError as err:
     print(f"Failure: {err}")
-finally:
-    print("whoops")
 
 # pulumi.export("digest", gunicorn_image.digest)
