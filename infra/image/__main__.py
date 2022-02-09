03bf59c2312fb5b7e9579de1b12112745255a813
Laura Santamaria <laura@nimbinatus.com>
2022-02-09 16:46:03 -0600

    fix(test): ok, print, if this is how we have to do this

--- infra/image/__main__.py
+++ infra/image/__main__.py
@@ -23,24 +23,25 @@ try:
     registry = gcp_native.artifactregistry.v1beta2.get_repository(
         repository_id=f'{gimage}'
     )
+    print(registry.name)
 except Exception as err:
     print(f'No artifact registry by that name: {err}. Spinning one up.')
-    registry = gcp_native.artifactregistry.v1beta2.Repository(
-        f'{gimage}',
-        location=f'{pulumi.Config("google-native").require("region")}',
-        repository_id=f'{gimage}',
-        description="repo for docker images for test run",
-        format=gcp_native.artifactregistry.v1beta2.RepositoryFormat("DOCKER"),
-    )
+    # registry = gcp_native.artifactregistry.v1beta2.Repository(
+    #     f'{gimage}',
+    #     location=f'{pulumi.Config("google-native").require("region")}',
+    #     repository_id=f'{gimage}',
+    #     description="repo for docker images for test run",
+    #     format=gcp_native.artifactregistry.v1beta2.RepositoryFormat("DOCKER"),
+    # )
 
-try:
-    gunicorn_image = docker.Image(
-        gimage,
-        build=f'{path.parents[1]}/api',
-        image_name=f'us-central-docker.pkg.dev/{pulumi.Config("google-native").require("project")}/{gimage}/{gimage}:{tag}',
-        opts=pulumi.ResourceOptions(depends_on=registry)
-    )
-except pulumi_docker.docker.ResourceError as err:
-    print(f"Failure: {err}")
+# try:
+#     gunicorn_image = docker.Image(
+#         gimage,
+#         build=f'{path.parents[1]}/api',
+#         image_name=f'us-central-docker.pkg.dev/{pulumi.Config("google-native").require("project")}/{gimage}/{gimage}:{tag}',
+#         opts=pulumi.ResourceOptions(depends_on=registry)
+#     )
+# except pulumi_docker.docker.ResourceError as err:
+#     print(f"Failure: {err}")
 
 # pulumi.export("digest", gunicorn_image.digest)
