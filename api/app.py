bacb201a2656b55002c360d109e52d6fd0f8fda6
Laura Santamaria <laura@nimbinatus.com>
2022-03-07 15:35:51 -0600

    Update app.py

--- api/app.py
+++ api/app.py
@@ -19,11 +19,11 @@ class Greeting(object):
     def on_get(self, req, resp):
         if "name" in req.params:
             payload = {
-                "response": f'hello, {req.params["name"]}'
+                "response": f'hello, {req.params["name"]}!'
             }
         else:
             payload = {
-                "response": "hello, world"
+                "response": "Hello, world!"
             }
         resp.text = json.dumps(payload)
         resp.status = falcon.HTTP_200
